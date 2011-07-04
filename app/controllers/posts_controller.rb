d4accbfa8016ca17eae4e7ad3afc15255f9e25c7
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-04 10:53:38 -0300

    scaffolding Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  # GET /posts
+  # GET /posts.xml
+  def index
+    @posts = Post.all
+    respond_with(@posts)
+  end
+
+  # GET /posts/1
+  # GET /posts/1.xml
+  def show
+    @post = Post.find(params[:id])
+    respond_with(@post)
+  end
+
+  # GET /posts/new
+  # GET /posts/new.xml
+  def new
+    @post = Post.new
+    respond_with(@post)
+  end
+
+  # GET /posts/1/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # POST /posts
+  # POST /posts.xml
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    respond_with(@post)
+  end
+
+  # PUT /posts/1
+  # PUT /posts/1.xml
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    respond_with(@post)
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.xml
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    respond_with(@post)
+  end
+end
