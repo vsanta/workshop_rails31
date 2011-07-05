768b80a23075789c510a2ecb9857d69628e91342
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-05 13:23:40 -0300

    hack to fix rails 3.1 rc4 bug where respond js is treated as xml like

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -5,4 +5,8 @@ class ApplicationResponder < ActionController::Responder
   # Uncomment this responder if you want your resources to redirect to the collection
   # path (index action) instead of the resource path for POST/PUT/DELETE requests.
   # include Responders::CollectionResponder
+
+  #have javascript response to behave as to_html. Fix for 3.1 rc4
+  #can be used for other formats like :to_iphone :to_html
+  alias :to_js :to_html
 end
