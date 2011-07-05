113467a13bf86fbcd27005b561617f5f1601eb83
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-05 13:20:53 -0300

    including hermes scopes and action to be able to use a active record with capybara's within

--- spec/support/acceptance_example_group.rb
+++ spec/support/acceptance_example_group.rb
@@ -4,6 +4,11 @@ module AcceptanceExempleGroup
   include Capybara::DSL
   include Capybara::RSpecMatchers
   include Rails.application.routes.url_helpers
+  #changes capybara within to work with Active record objects
+  include Hermes::Scopes
+  #adds select_datetime, select_time and friends
+  include Hermes::Actions
+
 
   included do
     metadata[:type] = :acceptance
