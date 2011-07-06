6049c337086526d1dd654c152ddb3be48a167847
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-06 11:05:39 -0300

    post validates user presence

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should raise an error if a user is not given" do
+     expect {
+      Factory.create(:post, :user => nil)
+     }.to raise_error RuntimeError, "@post.user is nil"
+    end
+  end
 end
