7845a632225b65c29fbe1b09d66770661ec3850e
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-06 10:10:18 -0300

    user migration ran

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :name
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  #attr accessible with role in 3.1 ex:  attr_accessible :email, :as=>:admin
+  #then User.new(params[:user], :as=>admin)
 end
