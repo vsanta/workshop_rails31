1da27ba1549636d71b9a0ed980b4a05fcf387a89
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-07-08 09:48:35 -0300

    adding slug flied to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707154148) do
+ActiveRecord::Schema.define(:version => 20110708124749) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "post_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20110707154148) do
     t.string   "location"
     t.float    "lat"
     t.float    "lng"
+    t.string   "slug"
   end
 
   create_table "users", :force => true do |t|
